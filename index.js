3ebf50fd4b4c3ef1cf57e2ff085a94b28731b769
Hilary Cordero Flores <h.cordero@WEB-07.local>
2019-06-22 17:34:57 -0600

    cambios al index.js

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ http.createServer(runserver).listen(5000);
 
 let brandJSON = require('../Tarea1-Coches/brand.json');
 
-function runserver(req,res) {
-// ####aqui muestro el json
+function runserver(req, res) {
+  // ####aqui muestro el json
   // if( req.method === 'GET' ) {
   //   res.end(JSON.stringify(brandJSON));
   // } 
@@ -12,38 +12,38 @@ function runserver(req,res) {
   //   res.end(JSON.stringify(brandJSON));
   // }
 
-// ############
+  // ############
 
-// #######muestro la url
-  if(req.url === '/api/v1/cars') {
-    if(req.method === 'GET') {
+  // #######muestro la url
+  if (req.url === '/api/v1/cars') {
+    if (req.method === 'GET') {
       res.end(JSON.stringify({
         success: true,
         error: null,
         data: brandJSON
       }));
-    } 
-    }else res.end(JSON.stringify({
-      success: false,
-      error: 'Error',
-      data: null
-    }));  
-    console.log(req.url);
-// #######muestro la url
+    }
+  } 
+  console.log(req.url);
+  // #######muestro la url
 
-// #######pasar a un formato csv
-if(req.url === '/api/v1/cars/key') {
-  let h = Object.keys(brandJSON[0]).join(',')+ "\n";
-  let body = '';
-  brandJSON.forEach(o => {
-    body += Object.keys(0).map(k => o[k].join(',')+ "\n");
-  });
-  h += body;
-  res.end(h);
-};  
+  // #######pasar a un formato csv
+  if (req.url === '/api/v1/cars/key') {
+    let h = Object.keys(brandJSON[0]).join(',') + "\n";
+    let body = '';
+    brandJSON.forEach(o => {
+      body += Object.keys(o).map(k => o[k]).join(',') + "\n";
+    });
+    h += body;
+    res.end(h);
+  } else res.end(JSON.stringify({
+    success: false,
+    error: 'Error',
+    data: null
+  }));
 
-// ######pasar a un formato csv
-  
+  // ######pasar a un formato csv
+
 
 }
 // rest.setHeader('Access-Control-Allow-Origin','*');
