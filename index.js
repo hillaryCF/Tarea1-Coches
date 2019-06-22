72d9b57002f4b1352e2713458395c6f91ca55044
Hilary Cordero Flores <h.cordero@WEB-07.local>
2019-06-22 16:28:25 -0600

    cambios al index.js

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 const http = require('http');
 http.createServer(runserver).listen(5000);
 
+let brandJSON = require('../Tarea1-Coches/brand.json');
+
 function runserver(req,res) {
-  if( req.method === 'GET' ) {
-    res.end('GET');
-  } 
-  if( req.method === 'POST') {
-    res.end('POST');
-  }
+  // if( req.method === 'GET' ) {
+  //   res.end(JSON.stringify(brandJSON));
+  // } 
+  // if( req.method === 'DELETE') {
+  //   res.end(JSON.stringify(brandJSON));
+  // }
+  if(req.url === '/api/v1/cars') {
+    if(req.method === 'GET') {
+      res.end(JSON.stringify({
+        success: true,
+        error: null,
+        data: brandJSON
+      }));
+    } 
+    }else res.end(JSON.stringify({
+      success: false,
+      error: 'Error',
+      data: null
+    }));
+
+  
+  console.log(req.url);
 }
